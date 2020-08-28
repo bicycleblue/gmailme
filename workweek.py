75cbc9cc9b7d347bdc10a2c71e7a100816d0bcd2
bicycleblue <greg.s.long@gmail.com>
2020-08-28 09:26:47 -0400

    add bingeclock noise to workweek

--- workweek.py
+++ workweek.py
@@ -2,6 +2,23 @@
 
 import gmailme
 import datetime
+from bingeclock import bingeclock_series
+import random
+
+
+series_list = [{"title": "Breaking Bad",
+                "url": "breaking-bad"},
+               {"title": "Avatar: The Last Airbender",
+                "url": "avatar-the-last-airbender"},
+               {"title": "It\'s Always Sunny in Philadelphia",
+                "url": "its-always-sunny-in-philadelphia"},
+               {"title": "Seinfeld",
+                "url": "seinfeld"},
+               {"title": "Friends",
+                "url": "friends-1994"},
+               {"title": "Veep",
+                "url": "veep"}
+              ]
 
 
 class WorkWeek(gmailme.GMailMe):
@@ -19,10 +36,20 @@ class WorkWeek(gmailme.GMailMe):
 
         delta_days = today - start_date
 
-        self.message = "Home since {}\n".format(start_date)
+        self.message = "Home since {}\n\n".format(start_date)
         self.message += "  {} days of isolation\n".format(delta_days.days)
-        self.message += "  {} weeks of isolation\n".format(str(round(delta_days.days / 7, 2)))
+        self.message += "  {} weeks of isolation\n\n".format(str(int(round(delta_days.days / 7, 2))))
 
+        series = random.choice(series_list)
+        self.logger.debug("chose series {}".format(series['title']))
+
+        clock = bingeclock_series(series['url'], hours=8)
+        if clock:
+            binge_days = clock[0]
+            times = int(round(delta_days.days / binge_days))
+            self.message += "If you had spent 8 hours a day binging on {},\nyou could have watched the entire series {} times!\n".format(series['title'], times)
+        else:
+            self.logger.debug("failed to get bingeclock for series {}".format(series['title']))
 
 if __name__ == "__main__":
     ww = WorkWeek()
